a8366d48d5b8cdbba8936ab7cc16bfd203a9b441
Pruthviraj Patil <pruthviraj.patil@goapptiv.in>
2023-11-21 13:32:02 +0530

    sheet update

--- app.js
+++ app.js
@@ -38,10 +38,66 @@ while ((match1 = docnopattern.exec(data.text)) !== null) {
     break;
   }
 }
+const regex = /CREDIT NOTE/;
+var j =0;
+
+while(data.text.match(regex))
+{
+  var match2 = data.text.match(regex);
+  var count = data.text.indexOf(match2[0]);
+  while(data.text[count]!='.')
+{
+  count++;
+}
+count+=3;
+var ans="";
+while(data.text[count]!=']')
+{
+  count++;
+ans+=data.text[count];
+}
+console.log(ans);
+extractedDates.push({ To : ans });
+var count = data.text.indexOf(match2[0]);
+j++;
+if(j>data.numpages)
+{
+  break;
+}
+}
+var regex2 = /Amount in words/;
+var match2 = data.text.match(regex2);
+var j=0;
+while(data.text.match(regex2)){
+  var match2 = data.text.match(regex2);
+var count = data.text.indexOf(match2[0])
+
+j++;
+count+=18;
+var ans=""
+while(data.text[count]!='.')
+{
+  ans+=data.text[count]
+  // console.log(data.text[count]);
+  count++;
+}
+console.log(ans);
+extractedDates.push({ GrandTotal : ans });
+if(j>data.numpages)
+{
+  break;
+}
+}
+
+const extractedDates2 = [];
   // Export extracted dates to Excel
   const ws = XLSX.utils.json_to_sheet(extractedDates);
+  const ws1 = XLSX.utils.json_to_sheet(extractedDates2);
+
   const wb = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
+  XLSX.utils.book_append_sheet(wb, ws1, 'Sheet 2');
+
   XLSX.writeFile(wb, 'extracted_dates.xlsx');
 
   console.log('Excel file "extracted_dates.xlsx" created successfully.');
