cd26af5435323d37a7b2a32928e427baafe2bce6
Pruthviraj Patil <pruthviraj.patil@goapptiv.in>
2023-11-21 17:15:05 +0530

    new logic of iteration

--- app.js
+++ app.js
@@ -5,98 +5,46 @@ const XLSX = require('xlsx');
 const pdffile = fs.readFileSync('one.pdf');
 
 pdfparse(pdffile).then(function (data) {
-  // Display the number of pages
   console.log('Number of Pages:', data.numpages);
-
-  // Display the text content
   console.log('Text Content:', data.text);
-
-  const dateRegex = /Dated\s+:(\d{2}-\d{2}-\d{4})/;
-  var docnopattern = /([A-Z]{9})+(\d{6})/;
-
-  let match;
-  var i = 0;
   const extractedDates = [];
 
-  while ((match = dateRegex.exec(data.text)) !== null) {
-    const currentDate = match[1];
-    console.log("Dated: " + currentDate);
-    extractedDates.push({ Dated: currentDate });
-    i = i + 1;
-    if (i >= data.numpages) {
-      break;
-    }
+  var dateAns;
+  var totalAns;
+for(let i=0;i<data.text.length-20;i++)
+{
+if(data.text[i]=='D' && data.text[i+1]=='a' && data.text[i+2]=='t' && data.text[i+4]=='d')
+{
+  dateAns = data.text.substr(i,18);
+}
+
+if(
+  data.text[i]=='A' && 
+  data.text[i+1]=='m' &&
+  data.text[i+2]=='o' &&
+  data.text[i+7]=='i' &&
+  data.text[i+8]=='n' 
+
+  
+  )
+{
+  var temp = "";
+  while(data.text[i+18]!='.')
+  {
+    temp+=data.text[i+18]
+    i++;
   }
-let match1;
-var i =0;
-while ((match1 = docnopattern.exec(data.text)) !== null) {
-  const currentDate = match1[0];
-  console.log("Remarks: " + currentDate);
-  extractedDates.push({ Remarks: currentDate });
-  i = i + 1;
-  if (i >= data.numpages) {
-    break;
-  }
-}
-const regex = /CREDIT NOTE/;
-var j =0;
+  totalAns=temp;
+  extractedDates.push({ Dated: dateAns, GrandTotal: totalAns });
 
-while(data.text.match(regex))
-{
-  var match2 = data.text.match(regex);
-  var count = data.text.indexOf(match2[0]);
-  while(data.text[count]!='.')
-{
-  count++;
-}
-count+=3;
-var ans="";
-while(data.text[count]!=']')
-{
-  count++;
-ans+=data.text[count];
-}
-console.log(ans);
-extractedDates.push({ To : ans });
-var count = data.text.indexOf(match2[0]);
-j++;
-if(j>data.numpages)
-{
-  break;
-}
-}
-var regex2 = /Amount in words/;
-var match2 = data.text.match(regex2);
-var j=0;
-while(data.text.match(regex2)){
-  var match2 = data.text.match(regex2);
-var count = data.text.indexOf(match2[0])
 
-j++;
-count+=18;
-var ans=""
-while(data.text[count]!='.')
-{
-  ans+=data.text[count]
-  // console.log(data.text[count]);
-  count++;
-}
-console.log(ans);
-extractedDates.push({ GrandTotal : ans });
-if(j>data.numpages)
-{
-  break;
 }
 }
-
-const extractedDates2 = [];
   // Export extracted dates to Excel
   const ws = XLSX.utils.json_to_sheet(extractedDates);
-  const ws1 = XLSX.utils.json_to_sheet(extractedDates2);
 
   const wb = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
-  XLSX.utils.book_append_sheet(wb, ws1, 'Sheet 2');
 
   XLSX.writeFile(wb, 'extracted_dates.xlsx');
 
